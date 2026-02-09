a6168f59254e85c90857225a91f052db52adfb3a
Kolawole <babatundeksamuel@gmail.com>
2026-02-09 13:56:15 +0100

    updated access token time

--- app/main.py
+++ app/main.py
@@ -44,13 +44,13 @@ os.makedirs(Upload_dir,exist_ok=True)
 async def read_index(request: Request):
     return templates.TemplateResponse("fastapi_practice.html", {"request": request, "title": "FastAPI Challenge"})
 
-@app.head("/")
-async def head_root():
-    return Response(status_code=200)
+# @app.head("/")
+# async def head_root():
+#     return Response(status_code=200)
 
-@app.get("/health")
-async def health():
-    return {"status":"healthy"}
+# @app.get("/health")
+# async def health():
+#     return {"status":"healthy"}
 
 #signupFOrm Data
 @app.post("/signup")
